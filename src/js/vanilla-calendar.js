f1ec7675663f1dabb7628e18f0ad3911191350bb
Mauro Marssola <marssolinha@hotmail.com>
2019-10-11 13:17:45 -0300

    Change month buttons submit forms if calendar is placed inside a form

--- src/js/vanilla-calendar.js
+++ src/js/vanilla-calendar.js
@@ -137,9 +137,9 @@ let VanillaCalendar = (function () {
         const createCalendar = function () {
             document.querySelector(opts.selector).innerHTML = `
             <div class="vanilla-calendar-header">
-                <button class="vanilla-calendar-btn" data-calendar-toggle="previous"><svg height="24" version="1.1" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"></path></svg></button>
+                <button type="button" class="vanilla-calendar-btn" data-calendar-toggle="previous"><svg height="24" version="1.1" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"></path></svg></button>
                 <div class="vanilla-calendar-header__label" data-calendar-label="month"></div>
-                <button class="vanilla-calendar-btn" data-calendar-toggle="next"><svg height="24" version="1.1" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path></svg></button>
+                <button type="button" class="vanilla-calendar-btn" data-calendar-toggle="next"><svg height="24" version="1.1" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path></svg></button>
             </div>
             <div class="vanilla-calendar-week"></div>
             <div class="vanilla-calendar-body" data-calendar-area="month"></div>
